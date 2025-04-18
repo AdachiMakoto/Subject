cbb168a904d700b5099a826be21078bd81747bac
AdachiMakoto <adachi.m22@gmail.com>
2025-04-18 15:13:40 +0900

    Delete variable

--- OutlineExtension/Plugins/OutlineRenderPipeline/Source/OutlineRenderPipeline/Public/AddMyShader.h
+++ OutlineExtension/Plugins/OutlineRenderPipeline/Source/OutlineRenderPipeline/Public/AddMyShader.h
@@ -9,16 +9,10 @@ struct FAddMyShaderInput
 {
 	FRDGTextureRef Target;
 	FRDGTextureRef SceneDepth;
-	FRDGTextureRef LineTexture;
 	FRDGTextureRef InputTexture;
 	FRDGTextureRef OutputTexture;
 	TRDGUniformBufferRef<FSceneTextureUniformParameters> SceneTextures;
 	FScreenPassTexture SceneColor;
-	FLinearColor LineColor;
-	int32 LineWidth;
-	// Test
-	// FScreenPassTexture SceneColor;
-	// FScreenPassRenderTarget OverrideOutput;
 };
 
 void AddComputePass(FRDGBuilder& GraphBuilder, const FViewInfo& View, const FAddMyShaderInput& Inputs);
